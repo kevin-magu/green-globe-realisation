2118ec1ab236c34c7f885c0ed7cd507b9889c022
kevin-magu <wanjaukevinmagu@gmail.com>
2023-06-09 17:36:47 +0300

    u

---
+++ app.js
@@ -0,0 +1,13 @@
+overviewDiv = document.querySelector(".overview");
+areasoffocuswDiv = document.querySelector(".areas-of-focus");
+patnershipsDiv = document.querySelector(".partnerships");
+visionmissionDiv = document.querySelector(".vision");
+corevalueswDiv = document.querySelector(".core-values");
+
+function displayOverviewDiv(){
+    overviewDiv.style.display="block";
+    areasoffocuswDiv.style.display="none";
+    patnershipsDi.style.display="none";
+    visionmissionDiv.style.display="none";
+    corevalueswDiv.style.display="none";
+}
