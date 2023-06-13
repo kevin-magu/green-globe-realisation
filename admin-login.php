629bd5993f7e3a2cd6f561c7d59cc967ec6405d9
kevin-magu <wanjaukevinmagu@gmail.com>
2023-06-13 20:26:11 +0300

    u

--- admin-login.php
+++ admin-login.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="admin.css">
-    <link rel="stylesheet" href="add-blog.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Blog</title>
+    <title>Document</title>
 </head>
 <body>
     
