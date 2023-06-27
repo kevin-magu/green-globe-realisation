654216e829882feefae7a8d68101a52fab3c7a7c
kevin-magu <wanjaukevinmagu@gmail.com>
2023-06-27 18:20:58 +0300

    U

--- admin-login-processing.php
+++ admin-login-processing.php
@@ -1,38 +1,6 @@
-<?php
-include 'includes.php';
-
-// Function to validate username and password
-function validateCredentials($username, $password) {
-    // Prepare the SQL statement with placeholders
-    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = :username");
-
-    // Bind the parameter values
-    $stmt->bindParam(':username', $username);
-
-    // Execute the query
-    $stmt->execute();
-
-    // Fetch the user record
-    $user = $stmt->fetch();
-
-    // Verify the password
-    if ($user && password_verify($password, $user['password'])) {
-        // Username and password are valid
-        return true;
-    }
-
-    // Invalid username or password
-    return false;
-}
-
-// Example usage
+<?php 
 $username = $_POST['username'];
 $password = $_POST['password'];
+echo "my name is " + $username + "and my password is "+ $password ;
 
-// Validate the credentials
-if (validateCredentials($username, $password)) {
-    echo "Login successful!";
-} else {
-    echo "Invalid username or password.";
-}
 ?>
